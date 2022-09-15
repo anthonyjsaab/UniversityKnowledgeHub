80b080d3a4046dede909a499974fd3f3df6a65a3
Anthony <ajs34@mail.aub.edu>
2022-09-15 20:23:19 +0300

    Added missing logout path

--- authentication/urls.py
+++ authentication/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
         f'https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id={AD_CLIENT_ID}'
         '&response_type=code&response_mode=query&scope=openid%20profile%20User.Read%20offline_access')),
     path('i_got_code/', views.validate_login),
-    path('test/', views.test)
+    path('test/', views.test),
+    path('logout/', views.logout),
 ]
