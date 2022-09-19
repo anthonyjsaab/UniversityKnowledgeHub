8934c7d2553a81a172ce7c82077c00407dbcc3ef
Anthony <ajs34@mail.aub.edu>
2022-09-19 08:47:00 +0300

    Now handling multiple django_sessions attached to the same microsoft_sessionid.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,15 +1,12 @@
-from django.contrib import admin
 from django.http import HttpResponseRedirect
 from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
-
 from UniversityKnowledgeHub.settings import AD_CLIENT_ID
 from authentication import views
 
 urlpatterns = [
     path('login/', lambda request: HttpResponseRedirect(
         f'https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id={AD_CLIENT_ID}'
-        '&response_type=code&response_mode=query&scope=openid%20profile%20User.Read%20offline_access')),
+        '&response_type=code&response_mode=query&scope=openid%20profile%20email%20offline_access%20User.Read')),
     path('i_got_code/', views.validate_login),
     path('test/', views.test),
     path('logout/', views.log_me_out),
--- authentication/views.py
+++ authentication/views.py
@@ -1,18 +1,17 @@
 import json
 import uuid
+import requests
 from django.contrib.auth import login, logout
 from django.contrib.sessions.models import Session
 from django.http import HttpResponse
 from django.shortcuts import render
-import requests
 from UniversityKnowledgeHub.settings import AD_CLIENT_ID, AD_CLIENT_SECRET
 from authentication.models import MyUser, SSOut
 
 
-# Create your views here.
 def validate_login(request):
     if not request.GET.get('code', []):
-        return HttpResponse("Login unsuccessful")
+        return HttpResponse("Login Unsuccessful")
     code, session_state = request.GET.get('code'), request.GET.get('session_state')
     data = {
         'client_id': AD_CLIENT_ID,
@@ -29,11 +28,11 @@ def validate_login(request):
     if str(response.status_code) != '200':
         return HttpResponse("Something went wrong 2")
     j = json.loads(response.content)
-    email = j.get('mail')
+    email = j.get('userPrincipalName')
     obj, created = MyUser.objects.get_or_create(email=email, defaults={'email': email, 'username': str(uuid.uuid4())})
     login(request, obj)
     session = Session.objects.get(session_key=request.session.session_key)
-    ssout = SSOut.objects.create(microsoft_sessionid=session_state, django_session=session, user=request.user)
+    SSOut.objects.create(microsoft_sessionid=session_state, django_session=session, user=request.user)
     return render(request, 'authentication/empty.html')
 
 
@@ -51,9 +50,10 @@ def log_me_out(request):
 
 def sso_logout(request):
     if 'sid' not in request.GET.keys() or not request.GET.get('sid'):
-        return HttpResponse('Bruh')
-    maybe = SSOut.objects.get(microsoft_sessionid=request.GET.get('sid'))
+        return HttpResponse("Ew")
+    maybe = SSOut.objects.filter(microsoft_sessionid=request.GET.get('sid'))
     if maybe:
-        maybe.django_session.delete()
+        for ele in maybe:
+            ele.django_session.delete()
     return HttpResponse("OK")
 
