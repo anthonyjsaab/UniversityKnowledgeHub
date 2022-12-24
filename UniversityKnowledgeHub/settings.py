9788be2ee445c214be5d7cb748b0b5bc6ea734b9
Anthony <ajs34@mail.aub.edu>
2022-12-24 16:55:32 +0200

    Django now uses AWS RDS, and project "migrated" to VSCode

--- UniversityKnowledgeHub/settings.py
+++ UniversityKnowledgeHub/settings.py
@@ -79,11 +79,22 @@ WSGI_APPLICATION = 'UniversityKnowledgeHub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+'''
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+'''
+
+DATABASES = {
+    'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': str(os.environ.get('RDS_DB_NAME')),
+       'USER': str(os.environ.get('RDS_DB_USER')),
+       'PASSWORD': str(os.environ.get('RDS_DB_PASSWORD')),
+       'HOST': str(os.environ.get('RDS_DB_HOST')),
+       'PORT': int(os.environ.get('RDS_DB_PORT')),
+   }
 }
 
 
